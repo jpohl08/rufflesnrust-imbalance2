ee7be47c54a8bcc5455e9a7ed856663821858c67
Jennie Pohl <jennie.schober@gmail.com>
2012-08-26 13:08:04 -0700

    updating footer to include take flight sites

--- footer.php
+++ footer.php
@@ -3,9 +3,9 @@
 	<div id="footer">
 		<div id="site-info">
 			© 2011 <a href="/"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a><br />
-			Designed by <a href="http://wpshower.com" target="_blank">Wpshower</a>
-			<span class="main_separator">/</span>
-			Powered by <a href="http://www.wordpress.org" target="_blank">WordPress</a>
+			Built by <a href="http://takeflightsites.com">Take Flight Sites</a> | 
+			<a href="http://wpshower.com" target="_blank">Wpshower</a> | 
+			<a href="http://www.wordpress.org" target="_blank">WordPress</a>
 		</div><!-- #site-info -->
 		<div id="footer-right"><?php wp_nav_menu( array( 'container_class' => 'menu', 'theme_location' => 'footer-right', 'walker' => new Imbalance2_Walker_Nav_Menu(), 'depth' => 1 ) ); ?></div>
 		<div id="footer-left"><?php wp_nav_menu( array( 'container_class' => 'menu', 'theme_location' => 'footer-left', 'walker' => new Imbalance2_Walker_Nav_Menu(), 'depth' => 1 ) ); ?></div>
